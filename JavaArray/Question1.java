8230fc44c3b04ba12e959c5b1fb49e452a1ae650
Naveen kumar <98445817+Naveen2334@users.noreply.github.com>
2023-05-07 15:13:27 +0530

    Add files via upload

---
+++ JavaArray/Question1.java
@@ -0,0 +1,26 @@
+package JavaArray;
+/*
+ * Q1. Given an array of integers {2, 6, -5, -1, 0, 4, -9}, print only the positive values present in the array.
+    Output:
+     2
+     6
+     0
+     4
+ */
+
+public class Question1 {
+	
+	
+	
+public static void main(String[] args) {
+	int arr[] = {2, 6, -5, -1, 0, 4, -9};
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]>=0)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	
+}
+}
